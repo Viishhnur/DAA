2fccb25d70e664ed4fed59665f5605b89fc26f32
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2024-11-07 23:19:14 +0530

    Added Day-19 and Day-20

---
+++ Day-19/Day19P1AllLonelyNodes.java
@@ -0,0 +1,41 @@
+/*
+In a joint family, every person assigned with an ID, an integer value.
+and the entire family is arranged in the from of tree.
+
+You will be given the family tree,
+your task is to find and print the person ID, who is only child to his/her parent.
+i.e, the person with no siblings. Print all such person ID's in ascending order.
+
+NOTE: 
+	- If only one person is there in the family, please consider him as a parent.
+	- In the given tree, please consider '-1' as null.
+
+Input Format:
+-------------
+Line-1: Space separated integers, person ID's in the family.
+
+Output Format:
+--------------
+Line-1: Print the list of person IDs in ascending order.
+
+
+Sample Input-1:
+---------------
+2 3 4 -1 5
+
+Sample Output-1:
+----------------
+[5]
+
+Sample Input-2:
+---------------
+11 99 88 77 -1 -1 66 55 -1 -1 44 33 -1 -1 22
+
+Sample Output-2:
+----------------
+[77, 66, 55, 44, 33, 22]
+
+ */
+public class Day19P1AllLonelyNodes {
+    
+}
